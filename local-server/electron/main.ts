7a20937677bf83996de9c94afa8b228174fe1780
WHIZ TECH <whiz.techke@gmail.com>
2026-09-18 14:11:20 +0300

    fix(local-server): set custom window icon in BrowserWindow options

--- local-server/electron/main.ts
+++ local-server/electron/main.ts
@@ -90,6 +90,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, '../assets/icon.ico'),
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
